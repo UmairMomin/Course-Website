17c57dbd9657553f78965423c68a2a122e1c688a
Umair Momin <umairmomin04@gmail.com>
2024-07-28 20:13:24 +0530

    Final webpage

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in',
+      },
+    },
+  },
+  plugins: [],
+}
+
